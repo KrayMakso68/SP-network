e4bb9dced57c12bbc2477023d1798d9cf370be92
KrayMakso68 <makson6803@gmail.com>
2024-05-13 17:29:52 +0300

    edit main window

--- main.py
+++ main.py
@@ -3,18 +3,16 @@ import customtkinter
 
 app = customtkinter.CTk()
 app.title("my app")
-app.geometry("800x600")
+app.geometry("600x400")
 
+tabview = customtkinter.CTkTabview(master=app)
+tabview.pack(expand=True, fill='both', padx=5, pady=5)
 
+tabview.add("Шифратор")  # add tab at the end
+tabview.add("Метод встречи по середине")  # add tab at the end
+tabview.set("Шифратор")  # set currently visible tab
 
-# tabview = customtkinter.CTkTabview(master=app)
-# tabview.pack(padx=20, pady=20)
-#
-# tabview.add("tab 1")  # add tab at the end
-# tabview.add("tab 2")  # add tab at the end
-# tabview.set("tab 2")  # set currently visible tab
-#
-# button = customtkinter.CTkButton(master=tabview.tab("tab 1"))
-# button.pack(padx=20, pady=20)
+button = customtkinter.CTkButton(master=tabview.tab("Шифратор"))
+button.pack(padx=20, pady=20)
 
 app.mainloop()
